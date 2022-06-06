0df393a2b02a4a9aa747df5889ee4a994563970c
Amirreza Aminian <aminian.amirreza@gmail.com>
2022-06-06 17:49:50 +0430

    Map has added

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// Amirreza Aminian
 
+'use strict' ;
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYW1pcnJlemEtYW1pbmlhbjgwODgiLCJhIjoiY2w0MnFjc2d5MHVubzNkbzY0bmZndnB1ZSJ9.P2J3RuurXm_dMFSwDeKc_w";
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+});
